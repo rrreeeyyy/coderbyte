aa2adcb80fc262180f6ba00d93a494b0fa80b16d
YOSHIKAWA Ryota <yoshikawa@rrreeeyyy.com>
2015-07-15 21:47:13 +0900

    Add letter_count_I.rb

---
+++ letter_count_I.rb
@@ -0,0 +1,13 @@
+def LetterCountI(str)
+  greatest_repeated = str.
+                        split(' ').
+                        sort_by { |word| repeat_count(word) }.
+                        last
+  repeat_count(greatest_repeated) == 1 ? -1 : greatest_repeated
+end
+
+def repeat_count(word)
+  word.count(word.split(//).max_by {|e| word.count(e) })
+end
+
+LetterCountI(STDIN.gets)
